c0b6768278709d8d06016e2a2bedc8de727f2ccb
uncle-sly <Unclesly@yandex.ru>
2023-12-28 22:33:43 +0200

    Sprint 3 dinner constructor Task solution

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,91 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class DinnerConstructor {
+
+    private  HashMap<String, ArrayList<String>> dishMenu;
+    private ArrayList<String> setOfMealTypes;
+
+    public DinnerConstructor() {
+        dishMenu = new HashMap<>();
+        setOfMealTypes = new ArrayList<>();
+    }
+
+    public ArrayList<String> getSetOfMealTypes() {
+        return setOfMealTypes;
+    }
+
+    public HashMap<String, ArrayList<String>> getDishMenu() {
+        return dishMenu;
+    }
+
+    boolean checkType(String type) {
+        for (String name : dishMenu.keySet()) {
+            if (name.equals(type)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void saveNewDish(String dishType, String dishName) {
+        ArrayList<String> dishNames = new ArrayList<>();
+
+        if (!dishMenu.isEmpty()) {
+            if (checkType(dishType)) {
+                dishNames = dishMenu.get(dishType);
+            }
+        }
+        dishNames.add(dishName);
+        dishMenu.put(dishType, dishNames);
+    }
+
+    public void saveSetOfMealTypes(String nextItem) {
+        setOfMealTypes.add(nextItem);
+    }
+
+    public void clearSetOfMealTypes() {
+        setOfMealTypes.clear();
+    }
+
+    public ArrayList<ArrayList<String>> createDishCombos(int numberOfCombos, Random random) {
+    /* public HashMap<String, ArrayList<String>> createDishCombos(int numberOfCombos, ArrayList<String> setOfMealTypes, Random random) {
+        HashMap<String, ArrayList<String>> dishCombos = new HashMap<>();
+        ArrayList<String> comboMeals;
+            for (int i = 0; i < numberOfCombos; i++) {
+                comboMeals = new ArrayList<>();
+                for (String type : getSetOfMealTypes()) {
+                    int index = random.nextInt(dishMenu.get(type).size());
+                    comboMeals.add(dishMenu.get(type).get(index));
+                    dishCombos.put("Комбо " + (i + 1), comboMeals);
+                }
+            }  */
+        ArrayList<ArrayList<String>> dishCombos = new ArrayList<>();
+        ArrayList<String> comboMeals;
+        for (int i = 0; i < numberOfCombos; i++) {
+            comboMeals = new ArrayList<>();
+            for (String type : getSetOfMealTypes()) {
+                int index = random.nextInt(dishMenu.get(type).size());
+                comboMeals.add(dishMenu.get(type).get(index));
+            }
+            dishCombos.add(comboMeals);
+        }
+        return dishCombos;
+    }
+
+    public void printSetOfCombos(ArrayList<ArrayList<String>> dishCombos) {
+/* public void printSetOfCombos(HashMap<String,ArrayList<String>> dishCombos)
+   for (String comboName : dishCombos.keySet() ) {
+            System.out.println(comboName);
+            System.out.println(dishCombos.get(comboName));
+        }*/
+        for (int i = 0; i < dishCombos.size(); i++) {
+            System.out.println("\nКомбо " + (i + 1));
+            System.out.println(dishCombos.get(i));
+        }
+    }
 
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,19 +1,22 @@
 package ru.practicum.dinner;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     static DinnerConstructor dc;
     static Scanner scanner;
+    static Random random;
 
     public static void main(String[] args) {
         dc = new DinnerConstructor();
         scanner = new Scanner(System.in);
+        random = new Random();
 
         while (true) {
             printMenu();
-            String command = scanner.nextLine();
+            String command = scanner.nextLine().trim();
 
             switch (command) {
                 case "1":
@@ -24,42 +27,83 @@ public class Main {
                     break;
                 case "3":
                     return;
+                default:
+                    System.out.println("Неизвестная команда, повторите ввод!\n");
             }
         }
     }
 
     private static void printMenu() {
-        System.out.println("Выберите команду:");
+        System.out.println("\nВыберите команду:");
         System.out.println("1 - Добавить новое блюдо");
         System.out.println("2 - Сгенерировать комбинации блюд");
-        System.out.println("3 - Выход");
+        System.out.println("3 - Выход \n");
     }
 
     private static void addNewDish() {
         System.out.println("Введите тип блюда:");
         String dishType = scanner.nextLine();
+        if (dishType.isEmpty()) {
+            System.out.println("тип блюда не может быть пустым, повторите ввод\n");
+            return;
+        }
+
         System.out.println("Введите название блюда:");
         String dishName = scanner.nextLine();
-
+        if (dishName.isEmpty()) {
+            System.out.println("название блюда не может быть пустым, повторите ввод\n");
+            return;
+        }
         // добавьте новое блюдо
+        dc.saveNewDish(dishType, dishName);
     }
 
     private static void generateDishCombo() {
-        System.out.println("Начинаем конструировать обед...");
+        if (dc.getDishMenu().isEmpty()) {
+            System.out.println("Меню блюд - пусто. Сначала добавьте блюда в меню.");
+            return;
+        }
+        System.out.println("Начинаем конструировать обед...\n");
 
         System.out.println("Введите количество наборов, которые нужно сгенерировать:");
         int numberOfCombos = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
+        if (numberOfCombos <= 0) {
+            System.out.println("Вы ввели: " + numberOfCombos + ". Для начала генерации, " +
+                               "количество наборов не может быть равно 0 или меньше 0.");
+            return;
+        }
+        if (dc.getSetOfMealTypes().isEmpty()) {
+            System.out.println("Вводите типы блюда из меню, разделяя символом переноса строки (enter). " +
+                               "Для завершения ввода введите пустую строку");
+        } else {
+            System.out.println("Ранее введенные типы блюд: " + dc.getSetOfMealTypes() +
+                               "\nДля очистки введите clear и нажмите Enter.");
+            System.out.println("Для генерации комбинаций блюд на основе ранее введенных типов, нажмите Enter. \n" +
+                               "Для добавления нового типа введите Имя типа и нажмите Enter. " +
+                               "\n\nДля завершения ввода нажмите Enter. ");
+        }
         String nextItem = scanner.nextLine();
 
         //реализуйте ввод типов блюд
         while (!nextItem.isEmpty()) {
-
+            if (nextItem.equals("clear")) {
+                dc.clearSetOfMealTypes();
+            } else {
+                if (!dc.checkType(nextItem)) {
+                    System.out.println("Вы ввели несуществующий тип блюда! Повторите ввод: ");
+                } else {
+                    dc.saveSetOfMealTypes(nextItem);
+                }
+            }
+            nextItem = scanner.nextLine();
         }
 
         // сгенерируйте комбинации блюд и выведите на экран
+        if (!dc.getSetOfMealTypes().isEmpty()) {
+            dc.printSetOfCombos(dc.createDishCombos(numberOfCombos, random));
+        }
 
     }
 }
